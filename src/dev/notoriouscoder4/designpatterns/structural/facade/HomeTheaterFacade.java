ac5ceb96d2c1e7d007a46b82ea02cf595c48f53c
Sawankumar <notoriouscoder4@gmail.com>
2023-11-08 14:58:22 +0530

    Facade Design Pattern is added.

---
+++ src/dev/notoriouscoder4/designpatterns/structural/facade/HomeTheaterFacade.java
@@ -0,0 +1,72 @@
+package dev.notoriouscoder4.designpatterns.structural.facade;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    StreamingPlayer player;
+    CdPlayer cd;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp,
+                             Tuner tuner,
+                             StreamingPlayer player,
+                             CdPlayer cd,
+                             Projector projector,
+                             TheaterLights lights,
+                             Screen screen,
+                             PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.player = player;
+        this.cd = cd;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("Get ready to watch a movie...");
+        popper.on();
+        popper.pop();
+        lights.dim(10);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        amp.on();
+        amp.setStreamingPlayer(player);
+        amp.setSurroundSound();
+        amp.setVolume(5);
+        player.on();
+        player.play(movie);
+    }
+
+    public void endMovie() {
+        System.out.println("Shutting movie theater down...");
+        popper.off();
+        lights.on();
+        screen.up();
+        projector.off();
+        amp.off();
+        player.stop();
+        player.off();
+    }
+
+    public void listenToRadio(double frequency) {
+        System.out.println("Tuning in the airwaves...");
+        tuner.on();
+        tuner.setFrequency(frequency);
+        amp.on();
+        amp.setVolume(5);
+        amp.setTuner(tuner);
+    }
+
+    public void endRadio() {
+        System.out.println("Shutting down the tuner...");
+        tuner.off();
+        amp.off();
+    }
+}
---
+++ src/dev/notoriouscoder4/designpatterns/structural/facade/Screen.java
@@ -0,0 +1,22 @@
+package dev.notoriouscoder4.designpatterns.structural.facade;
+
+public class Screen {
+    String description;
+
+    public Screen(String description) {
+        this.description = description;
+    }
+
+    public void up() {
+        System.out.println(description + " going up");
+    }
+
+    public void down() {
+        System.out.println(description + " going down");
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
